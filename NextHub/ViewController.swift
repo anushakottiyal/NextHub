c854472c63a35de1f66d73c1c2c5568012d3f437
Anusha Kottiyal <kanushak89@gmail.com>
2019-07-24 09:53:42 -0400

    Initial Commit

---
+++ NextHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NextHub
+//
+//  Created by Anusha Kottiyal on 7/24/19.
+//  Copyright © 2019 NextDigit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
